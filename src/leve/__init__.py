328129b569fafb57e8a921b696741a03d8d0c073
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-02-05 11:10:56 +0530

    i think its enough for a first proto

---
+++ src/leve/__init__.py
@@ -0,0 +1,12 @@
+from .main import edit_distance
+from .utils import *
+
+
+def hello() -> str:
+    return "Hello from leve!"
+
+
+__help__ = "A module to work with string simmilarity algorithm, written in pure python"
+
+
+__version__ = "0.1.0"
